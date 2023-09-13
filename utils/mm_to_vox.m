59445106cd25ad9e79e554c0a8fe66664ed3d746
sitiny <39376122+sitiny@users.noreply.github.com>
2023-09-13 13:35:36 +0100

    Create mm_to_vox.m

---
+++ utils/mm_to_vox.m
@@ -0,0 +1,14 @@
+function vox_coords = mm_to_vox(nifti_img, mm_coords)
+% takes a 3x1 matrix of mm coordinates and converts to voxel coordinates
+% requires nifti image file for affine transform
+% returns a 3x1 matrix of voxel coordinates
+
+header=niftiinfo(nifti_img);
+affine=header.Transform.T';
+% check mmcoords is 3x1 rather than 1x3
+if size(mm_coords,2) == 3
+    mm_coords = mm_coords';
+end
+
+vox_coords = affine\[mm_coords;1];
+vox_coords = vox_coords(1:3);
